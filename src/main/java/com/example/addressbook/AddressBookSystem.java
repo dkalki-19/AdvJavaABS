80fd18cd60392a5442700adbd33d055c4483b4e5
dkalki-19 <dkalkicdhandapani@gmail.com>
2025-08-26 11:29:35 +0530

    UC8: Search person in city/state

--- src/main/java/com/example/addressbook/AddressBookSystem.java
+++ src/main/java/com/example/addressbook/AddressBookSystem.java
@@ -12,5 +12,13 @@ public class AddressBookSystem {
     public AddressBook getAddressBook(String name) {
         return addressBooks.get(name);
     }
+    
+    public List<Contact> searchPersonInCityOrState(String key) {
+        return addressBooks.values().stream()
+                .flatMap(ab -> ab.getContacts().stream())
+                .filter(c -> c.getCity().equalsIgnoreCase(key) || c.getState().equalsIgnoreCase(key))
+                .toList();
+    }
+
 }
 
